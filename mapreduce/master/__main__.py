bf88078bdb66aaeff9e22f9ff783d442f1b4c935
batjin <batjin@umich.edu>
2020-11-04 05:23:05 +0800

    register almost done

--- mapreduce/master/__main__.py
+++ mapreduce/master/__main__.py
@@ -6,6 +6,7 @@ import time
 import click
 import socket
 import mapreduce.utils
+import pathlib
 
 
 # Configure logging
@@ -13,119 +14,162 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class Master:
+    # Member Variable
+    worker_dict = dict()
     def __init__(self, port):
         # Create tmp folder
-        # Create new thread - listens for heartbeats
+        # use pathlib with Parents = True and Exist_ok = True
+        """ cwd = os.getcwd()
+        dir = cwd/'tmp'
+        dir.mkdir(parents=True, exist_ok=True) """
+        
+        # Create new thread - listens for heartbeats on port-1
+        # Have a dictionary of workers each having a status
+        # in that thread make a socket on port-1
+        # for loop that checkes for messages from each workers periodically
+            # if after a certain period you dont receive any signal, mark that worker dead
+            # upadte the status
+
         # Create additional threads  (fault tolerance)
 
         # Create new socket on port number, call listen() 
-        thread = threading.Thread(target=listen, args=(port,))
-        thread.start()
-        time.sleep(100) # This gives up execution to the 'listen' thread
-        thread.join()  # Wait for listen thread to shut down
-        print("main() shutting down")
-        
+        thread = threading.Thread(target=self.listen, args=[port])
+        thread.start() # This gives up execution to the 'listen' thread
+        thread.join() # Wait for listen thread to shut down
 
-        # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register",
-            "worker_host": "localhost",
-            "worker_port": 6001,
-            "worker_pid": 77811
-        }
-        logging.debug("Master:%s received\n%s",
-            port,
-            json.dumps(message_dict, indent=2),
-        )
-        
+        print("main() shutting down")
+
+
         # TODO: you should remove this. This is just so the program doesn't
         # exit immediately!
         logging.debug("IMPLEMENT ME!")
         time.sleep(120)
 
-def listen(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("localhost", port))
-    sock.listen()
+    def listen(self, port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("localhost", port))
+        sock.listen()
 
-    # Wait for incoming messages
-        # Ignore invalid messages, try: block 
-    # Return once all threads are exited 
-    
-    
-    # Connect to a client
-    sock.settimeout(1)
+        # Wait for incoming messages
+            # Ignore invalid messages, try: block 
+        # Return once all threads are exited 
 
-    while True:
-        print("listening")
+        # Connect to a client
+        sock.settimeout(1)
 
-        # Listen for a connection for 1s.  The socket library avoids consuming
-        # CPU while waiting for a connection.
-        try:
-            clientsocket, address = sock.accept()
-        except socket.timeout:
-            continue
-        print("Connection from", address[0])
-
-        # Receive data, one chunk at a time.  If recv() times out before we can
-        # read a chunk, then go back to the top of the loop and try again.
-        # When the client closes the connection, recv() returns empty data,
-        # which breaks out of the loop.  We make a simplifying assumption that
-        # the client will always cleanly close the connection.
-        message_chunks = []
         while True:
+
+            # Listen for a connection for 1s.  The socket library avoids consuming
+            # CPU while waiting for a connection.
             try:
-                data = clientsocket.recv(4096)
+                clientsocket, address = sock.accept()
             except socket.timeout:
                 continue
-            if not data:
-                break
-            message_chunks.append(data)
-        clientsocket.close()
+            print("Connection from", address[0])
 
-        # Decode list-of-byte-strings to UTF8 and parse JSON data
-        message_bytes = b''.join(message_chunks)
-        message_str = message_bytes.decode("utf-8")
-        message_dict = json.loads(message_str)
-        print(message_dict)
-        # If Shutdown Message
-        if message_dict['message_type']:
-            if message_dict["message_type"] == "shutdown":
-                # shutdown()
-                print("Shutting Down.")
-                clientsocket.close()
-                return
-                
-            elif message_dict["message_type"] == "new_master_job":
-                # Master  Job Shit
+            # Receive data, one chunk at a time.  If recv() times out before we can
+            # read a chunk, then go back to the top of the loop and try again.
+            # When the client closes the connection, recv() returns empty data,
+            # which breaks out of the loop.  We make a simplifying assumption that
+            # the client will always cleanly close the connection.
+            message_chunks = []
+            while True:
+                try:
+                    data = clientsocket.recv(4096)
+                except socket.timeout:
+                    continue
+                if not data:
+                    break
+                message_chunks.append(data)
+            clientsocket.close()
 
-            elif message_dict["message_type"] == "register_ack":
-                # Register Ack Shit
+            # Decode list-of-byte-strings to UTF8 and parse JSON data
+            message_bytes = b''.join(message_chunks)
+            message_str = message_bytes.decode("utf-8")
+            message_dict = json.loads(message_str)
+            print(message_dict)
             
-        else:
-            print("ERROR. INVALID REQUEST")
-            continue 
-        print(message_dict)
+            # If Received Message
+            if message_dict['message_type']:
+                print("received message_type")
+                # If Shutdown Message
+                if message_dict["message_type"] == "shutdown":
+                    # shutdown()
+                    # kill each worker then self                 
+                    context = {
+                        "message_type": "shutdown"
+                    }
+                    # Loop through Workers
+                    for worker in self.worker_dict:
+                        # Skip if dead
+                        if worker["status"] == "dead":
+                            continue
 
-    print("listen() shutting down")
+                        # Else Send Shutdown Message
+                        sendSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        sendSock.connect(("localhost", worker_port))
+                        message = json.dumps(context)
+                        sendSock.sendall(message.encode('utf-8'))
+                        print("Sent: \n")
+                        print(message)
+                        print("To worker " + worker["worker_port"])
+                        
+                    print("Shutting Down.")
+                    clientsocket.close()
+                    return
 
-    # Shutdown
-    # if message_dict["message_type"] == "shutdown":
-        #for worker in sorted(workers):
-        
-         #   kill worker
-        #kill self
+                elif message_dict["message_type"] == "new_master_job":
+                    # Master Job Shit
+                    continue
 
-        # Send same shutdown message to workers
-            # Worker can complete task, then kill
-        # Kill self
+                elif message_dict["message_type"] == "register":
+                    print("Received: \n")
+                    print(json.dumps(message_dict))
+                    worker_port =  message_dict["worker_port"]
+                    worker_pid =  message_dict["worker_pid"]
+                    context = {
+                        "message_type": "register_ack",
+                        "worker_host": 'locahost',
+                        "worker_port": worker_port,
+                        "worker_pid" : worker_pid
+                    }
+                    # Send to worker
+                    """  sendSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sendSock.connect(("localhost", worker_port))
+                    message = json.dumps(context)
+                    sendSock.sendall(message.encode('utf-8')) """
+                    """ print("Sent: \n")
+                    print(message) """
+                    # Add worker to container
+                    self.worker_dict[worker_pid] = {"worker_port": worker_port, "status": "ready"}
+                    continue
+                
+            else:
+                print("ERROR. INVALID REQUEST")
+                continue 
+            print(message_dict)
+
+        print("listen() shutting down")
+
+        # Shutdown
+        # if message_dict["message_type"] == "shutdown":
+            #for worker in sorted(workers):
+            
+            #   kill worker
+            #kill self
+
+            # Send same shutdown message to workers
+                # Worker can complete task, then kill
+            # Kill self
 
 
 @click.command()
 @click.argument("port", nargs=1, type=int)
 def main(port):
     Master(port)
+    # Learn to kill yourself and the workers
+    # possibly the answer is process.terminate
 
 
 if __name__ == '__main__':
--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -1,5 +1,7 @@
 import os
 import logging
+import threading
+import socket
 import json
 import time
 import click
@@ -13,25 +15,124 @@ logging.basicConfig(level=logging.DEBUG)
 class Worker:
     def __init__(self, master_port, worker_port):
         logging.info("Starting worker:%s", worker_port)
-        logging.info("Worker:%s PWD %s", worker_port, os.getcwd())
+        logging.info("Worker:%s PWD %s", worker_port, os.getcwd()) 
 
-        # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register_ack",
-            "worker_host": "localhost",
-            "worker_port": 6001,
-            "worker_pid": 77811
-        }
-        logging.debug(
-            "Worker:%s received\n%s",
-            worker_port,
-            json.dumps(message_dict, indent=2),
-        )
+        # Get workers Process ID
+        pid = os.getpid()
+
+        # Create TCP Socket on worker_port, call listen()
+        thread = threading.Thread(target=self.listen, args=[master_port, worker_port])
+        thread.start() # This gives up execution to the 'listen' thread
+        # call functions
+        thread.join() # Wait for listen thread to shut down
+        
+        # Send register to master 
+        # When receive register_ack, create new thread
+            # Sends heartbeat messages to master 
 
         # TODO: you should remove this. This is just so the program doesn't
         # exit immediately!
         logging.debug("IMPLEMENT ME!")
         time.sleep(120)
+
+    def listen(self,master_port, worker_port):
+        isRegistered = False
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("localhost", worker_port))
+        sock.listen()
+
+        # Send Register Message to Master
+        sendSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sendSock.connect(("localhost", master_port))
+        context = {
+            "message_type" : "register",
+            "worker_host" : "localhost",
+            "worker_port" : worker_port,
+            "worker_pid" : os.getpid()
+        }
+        message = json.dumps(context)
+        sendSock.sendall(message.encode('utf-8'))
+        print("just sent register")
+        
+        # receive register_ack, then create thread f or heartbeat sending
+
+        # Connect to a client
+        sock.settimeout(1)
+
+        while True:
+            # Listen for a connection for 1s.  The socket library avoids consuming
+            # CPU while waiting for a connection.
+            try:
+                clientsocket, address = sock.accept()
+            except socket.timeout:
+                continue
+            print("Connection from", address[0])
+
+            # Receive data, one chunk at a time.  If recv() times out before we can
+            # read a chunk, then go back to the top of the loop and try again.
+            # When the client closes the connection, recv() returns empty data,
+            # which breaks out of the loop.  We make a simplifying assumption that
+            # the client will always cleanly close the connection.
+            message_chunks = []
+            while True:
+                try:
+                    data = clientsocket.recv(4096)
+                except socket.timeout:
+                    continue
+                if not data:
+                    break
+                message_chunks.append(data)
+            clientsocket.close()
+
+            # Decode list-of-byte-strings to UTF8 and parse JSON data
+            message_bytes = b''.join(message_chunks)
+            message_str = message_bytes.decode("utf-8")
+            message_dict = json.loads(message_str)
+            print(message_dict)
+            
+
+            # If Received Message
+            if message_dict['message_type']:
+                # Check registration, check for register ack
+                if not isRegistered: 
+                    if message_dict["message_type"] == "register_ack":
+                        isRegistered = True
+            
+                # If Shutdown Message
+                elif message_dict["message_type"] == "shutdown":
+                    # shutdown()
+                    # kill self
+                    print("Shutting Down.")
+                    clientsocket.close()
+                    return
+
+                elif message_dict["message_type"] == "new_worker_job":
+                    # Master  Job Shit
+                    continue
+                
+
+                    continue
+                
+            else:
+                print("ERROR. INVALID REQUEST")
+                continue 
+            print(message_dict)
+
+        print("listen() shutting down")
+
+        # Shutdown
+        # if message_dict["message_type"] == "shutdown":
+            #for worker in sorted(workers):
+            
+            #   kill worker
+            #kill self
+
+            # Send same shutdown message to workers
+                # Worker can complete task, then kill
+            # Kill self
+
 
 
 @click.command()
